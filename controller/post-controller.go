b8258649fbaf82027d093e7b57e759e360183986
quantacake <teddiepenbrock@gmail.com>
2020-12-15 12:13:33 -0600

    Implement GetByID function for redis

--- controller/post-controller.go
+++ controller/post-controller.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
+	"github.com/cmd-ctrl-q/go-mux-crash-course/cache"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/entity"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/errors"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/service"
@@ -14,17 +17,20 @@ type controller struct{}
 var (
 	// postService service.PostService = service.NewPostService() // old
 	postService service.PostService // new, now added to NewPostController
+	postCache   cache.PostCache     // new post cache object
 )
 
 // PostController ...
 type PostController interface {
 	GetAllPosts(w http.ResponseWriter, r *http.Request)
 	AddPost(w http.ResponseWriter, r *http.Request)
+	GetPostByID(w http.ResponseWriter, r *http.Request)
 }
 
 // NewPostController ...
-func NewPostController(service service.PostService) PostController {
-	postService = service
+func NewPostController(service service.PostService, cache cache.PostCache) PostController {
+	postService = service // set global service with postService in param
+	postCache = cache     // set global cache with postCache in param
 	return &controller{}
 }
 
@@ -45,7 +51,34 @@ func (*controller) GetAllPosts(w http.ResponseWriter, r *http.Request) {
 
 	// encode the data and send it back to client
 	json.NewEncoder(w).Encode(posts)
+}
 
+// GetPostByID from service and store value into cache. (faster to get post from cache if possible)
+func (*controller) GetPostByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the id from the url
+	postID := strings.Split(r.URL.Path, "/")[2]
+
+	// cache: check/get data from the postID
+	var post *entity.Post = postCache.Get(postID)
+	if post == nil {
+		n, _ := strconv.ParseInt(postID, 10, 64)
+		post, err := postService.FindByID(n)
+		if err != nil {
+			// cannot find id in db
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(errors.ServiceError{Message: "No posts found!"})
+			return
+		}
+		// store value into cache. Set(key, value)
+		postCache.Set(postID, post)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(post)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(post)
+	}
 }
 
 func (*controller) AddPost(w http.ResponseWriter, r *http.Request) {
--- controller/post-controller_test.go
+++ controller/post-controller_test.go
@@ -6,48 +6,52 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
+	"github.com/cmd-ctrl-q/go-mux-crash-course/cache"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/entity"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/repository"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/service"
 	"github.com/stretchr/testify/assert"
 )
 
-// type MockRepository struct {
-// 	mock.Mock
-// }
-
-// func (mock *MockRepository) GetAllPosts(w http.ResponseWriter, r *http.Request) {
-// 	args := mock.Called()
-// 	result := args.Get(0)
-
-// 	// return result.([]entity.Post), args.Error(1)
-// }
-
-// func (mock *MockRepository) AddPost(w http.ResponseWriter, r *http.Request) {
-// 	args := mock.Called()
-// 	result := args.Get(0)
-
-// 	// return result.([]entity.Post), args.Error(1)
-// }
-
 const (
+	ID    int64  = 123
 	TITLE string = "Title 1"
 	TEXT  string = "Text 1"
 )
 
 var (
-	postRepo       repository.PostRepository = repository.NewSQLiteRepository()
+	postRepo       repository.PostRepository = repository.NewPostgresRepository()
 	postSrv        service.PostService       = service.NewPostService(postRepo)
-	postController PostController            = NewPostController(postSrv)
+	postCacheSrv   cache.PostCache           = cache.NewRedisCache("localhost:6379", 0, 10)
+	postController PostController            = NewPostController(postSrv, postCacheSrv)
 )
+
+func setup() {
+	var post entity.Post = entity.Post{
+		ID:    ID,
+		Title: TITLE,
+		Text:  TEXT,
+	}
+	postRepo.Save(&post)
+}
+
+func tearDown(postID int64) {
+	var post entity.Post = entity.Post{
+		ID: postID,
+	}
+	postRepo.Delete(&post)
+}
 
 func TestAddPost(t *testing.T) {
 
+	setup()
+
 	// TODO (steps):
 	// 1. Create a new HTTP Post request
-	var jsonData = []byte(`{"title": "` + TITLE + `", "text": "` + TEXT + `"}`)
+	var jsonData = []byte(`{"title": "` + TITLE + `", "txt": "` + TEXT + `"}`)
 	// create new post request
 	req, _ := http.NewRequest("POST", "/posts", bytes.NewBuffer(jsonData))
 
@@ -71,12 +75,77 @@ func TestAddPost(t *testing.T) {
 	var post entity.Post
 	json.NewDecoder(io.Reader(resp.Body)).Decode(&post) // cast resp.Body to a Reader type
 
-	// Assert HTTP response
-	assert.NotNil(t, post.ID) // check if identifier is not nil
+	// Assert HTTP response - make an assertion on the data of the post
+	assert.NotNil(t, ID, post.ID) // check if identifier is not nil
 	assert.Equal(t, TITLE, post.Title)
 	assert.Equal(t, TEXT, post.Text)
+
+	// clean up database
+	tearDown(ID)
 }
 
+// Doesnt work
 func TestGetAllPosts(t *testing.T) {
 
+	// Insert new post
+	setup()
+
+	// Create new HTTP request
+	req, _ := http.NewRequest("GET", "/posts", nil)
+
+	// Assign HTTP Requesthandler function (controller function)
+	handler := http.HandlerFunc(postController.GetAllPosts)
+	// Record the HTTP Response
+	response := httptest.NewRecorder()
+	// Dispatch the HTTP Request
+	handler.ServeHTTP(response, req)
+
+	// Assert HTTP status
+	status := response.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
+
+	// Decode HTTP respones
+	var posts []entity.Post
+	json.NewDecoder(io.Reader(response.Body)).Decode(&posts)
+
+	// Assert HTTP response
+	assert.Equal(t, ID, posts[0].ID)
+	assert.Equal(t, TITLE, posts[0].Title)
+	assert.Equal(t, TEXT, posts[0].Text)
+}
+
+func TestGetPostByID(t *testing.T) {
+
+	// insert new post
+	setup()
+
+	// Create new HTTP request
+	req, _ := http.NewRequest("GET", "/posts/"+strconv.FormatInt(ID, 10), nil)
+
+	// Assign HTTP Requesthandler function (controller function)
+	handler := http.HandlerFunc(postController.GetPostByID)
+	// Record the HTTP Response
+	response := httptest.NewRecorder()
+	// Dispatch the HTTP Request
+	handler.ServeHTTP(response, req)
+
+	// Assert HTTP status
+	status := response.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
+
+	// Decode HTTP respones
+	var post entity.Post
+	json.NewDecoder(io.Reader(response.Body)).Decode(&post)
+
+	// Assert HTTP response
+	assert.Equal(t, ID, post.ID)
+	assert.Equal(t, TITLE, post.Title)
+	assert.Equal(t, TEXT, post.Text)
+
+	// Cleanup database
+	tearDown(ID)
 }
