d1d6a9d37a4607a86686062a0dd957bd9d0a267a
quantacake <teddiepenbrock@gmail.com>
2020-12-14 18:17:41 -0600

    Implement db.go in config directory

--- config/db.go
+++ config/db.go
@@ -3,13 +3,17 @@ package config
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB
+var (
+	DB  *sql.DB
+	err error
+)
 
 func init() {
-	var err error
-	DB, err = sql.Open("postgres", "postgres://postgres:password@localhost/astore?sslmode=disable")
+	DB, err = sql.Open("postgres", "postgres://postgres:password@localhost/muxdb?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
--- repository/postgres-repo.go
+++ repository/postgres-repo.go
@@ -1,51 +1,32 @@
 package repository
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 
+	"github.com/cmd-ctrl-q/go-mux-crash-course/config"
 	"github.com/cmd-ctrl-q/go-mux-crash-course/entity"
 	_ "github.com/lib/pq"
 )
 
 type postgresRepo struct{}
 
-var (
-	db  *sql.DB
-	err error
-)
-
-func init() {
-	db, err = sql.Open("postgres", "postgres://postgres:password@localhost/muxdb?sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-
-	if err = db.Ping(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Database connection successful")
-}
-
 // NewPostgresRepository ...
 func NewPostgresRepository() PostRepository {
 
-	// db, err = sql.Open("postgres", "./posts.db")
-	// connect to db
-	db, err = sql.Open("postgres", "postgres://postgres:password@localhost/muxdb?sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	// DB, err = sql.Open("postgres", "./posts.db")
+	// connect to DB
+	// DB, err = sql.Open("postgres", "postgres://postgres:password@localhost/muxdb?sslmode=disable")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer DB.Close()
 
 	// delete table in database
-	_, err := db.Exec("DROP TABLE posts;")
+	_, err := config.DB.Exec("DROP TABLE posts;")
 
 	sqlStmt := `CREATE TABLE posts (id BIGINT PRIMARY KEY NOT NULL, title TEXT NOT NULL, txt TEXT); DELETE FROM posts;`
 
-	_, err = db.Exec(sqlStmt)
+	_, err = config.DB.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 	}
@@ -54,21 +35,14 @@ func NewPostgresRepository() PostRepository {
 
 func (*postgresRepo) Save(post *entity.Post) (*entity.Post, error) {
 
-	db, err = sql.Open("postgres", "postgres://postgres:password@localhost/muxdb?sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	defer db.Close()
-
-	_, err := db.Exec("INSERT INTO posts (id, title, txt) VALUES ($1, $2, $3);", post.ID, post.Title, post.Text)
+	_, err := config.DB.Exec("INSERT INTO posts (id, title, txt) VALUES ($1, $2, $3);", post.ID, post.Title, post.Text)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
 
-	// NOTE: Tx transaction doesnt work
-	// tx, err := db.Begin()
+	// Tx (transaction) doesnt work
+	// tx, err := DB.Begin()
 	// if err != nil {
 	// 	log.Fatal(err)
 	// 	return nil, err
@@ -93,7 +67,7 @@ func (*postgresRepo) Save(post *entity.Post) (*entity.Post, error) {
 // FindAll TODO
 func (*postgresRepo) FindAll() ([]entity.Post, error) {
 
-	rows, err := db.Query("SELECT * FROM posts;")
+	rows, err := config.DB.Query("SELECT * FROM posts;")
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
